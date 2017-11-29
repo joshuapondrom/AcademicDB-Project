f6f88f27326b46628039ea029a28d1f0c541a2bb
joshuapondrom <japrkf@mst.edu>
2017-11-29 16:35:25 -0600

    More features added

--- webpage.py
+++ webpage.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
+@app.route('/professor')
+def professor():
+    return render_template('professor.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
