d6ceb4ba672ded868e389f47f9bc3d24c1054b80
joshuapondrom <japrkf@mst.edu>
2017-11-29 16:53:11 -0600

    added a 'about' page

--- webpage.py
+++ webpage.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/student')
 def student():
     return render_template('student.html')
