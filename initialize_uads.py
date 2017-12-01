07a3704b9032f8f29d0c7f23afa9ed9e9df9b6e2
Derek_Hymer <dahb52@mst.edu>
2017-12-01 11:10:33 -0600

    Reformatted the file as Doug had suggested. Not sure what to do about the Emergency Contact table since ID could refrence two diffrent tables. Perhaps we should make a new table?

--- initialize_uads.py
+++ initialize_uads.py
@@ -6,77 +6,104 @@ cursor.execute('CREATE TABLE Student('
                '    address VARCHAR(100),'
                '    Name VARCHAR(30),'
                '    Year INTEGER'
-               ')'
-               'CREATE TABLE Building('
+               ')')
+connection.commit()
+cursor.execute('CREATE TABLE Building('
                '    address VARCHAR(30) PRIMARY KEY,'
                '    Name VARCHAR(30),'
                '    Abbr VARCHAR(5)
                ')'
-               'CREATE TABLE Instructor('
+cursor.execute('CREATE TABLE Instructor('
                '    ID INTEGER PRIMARY KEY,'
                '    address VARCHAR(100),'
                '    Name VARCHAR(30),'
-               '    Dept VARCHAR(30)'
-               ')'
-               'CREATE TABLE Located('
+               '    Dept VARCHAR(30),'
+               '    FOREIGN KEY(Dept) REFRENCES Department(Program)'
+               ')')
+connection.commit()
+cursor.execute('CREATE TABLE Located('
                '    Program VARCHAR(30),'
                '    Address VARCHAR(100),'
-               '    PRIMARY KEY(Program,Address)'
-               ')'
-               'CREATE TABLE Room('
+               '    PRIMARY KEY(Program,Address),'
+               '    FOREIGN KEY(Program) REFRENCES Department(Program),'
+               '    FOREIGN KEY(Address) REFRENCES Building(address)'
+               ')')
+connection.commit()
+cursor.execute('CREATE TABLE Room('
                '    Type VARCHAR (20),'
                '    Room_Number INTEGER,'
                '    Address VARCHAR(100),'
-               '    PRIMARY KEY(Room_Number, Address)'
-               ')'
-               'CREATE TABLE Class('
+               '    PRIMARY KEY(Room_Number, Address),'
+               '    FOREIGN KEY(Address) REFRENCES Building(address)'
+               ')')
+connection.commit()
+cursor.execute('CREATE TABLE Class('
                '    Class_ID CHAR(5),'
                '    DayCode CHAR(5),'
                '    Time INTEGER,'
                '    Course_ID VARCHAR(15),'
-               '    PRIMARY KEY(Course_ID, Class_ID)'
-               ')'
-               'CREATE TABLE Course('
+               '    PRIMARY KEY(Course_ID, Class_ID),'
+               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               ')')
+connection.commit()
+cursor.execute('CREATE TABLE Course('
                '    Course_ID VARCHAR(15) PRIMARY KEY,'
                '    Major VARCHAR(30),'
                '    Credit_Hourse INTEGER'
-               ')'
-               'CREATE TABLE Takes('
+               ')')
+connection.commit()
+cursor.execute('CREATE TABLE Takes('
                '    ID INTEGER,'
                '    Class_ID CHAR(5),'
                '    Course_ID VARCHAR(15),'
                '    Grade FLOAT,'
-               '    PRIMARY KEY(ID, Class_ID, Course_ID'
-               ')'
-               'CREATE TABLE In('
+               '    PRIMARY KEY(ID, Class_ID, Course_ID),'
+               '    FOREIGN KEY(ID) REFRENCES Student(ID),'
+               '    FOREIGN KEY(Class_ID) REFRENCES Class(Class_ID),'
+               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               ')')
+connection.commit()               
+cursor.execute('CREATE TABLE In('
                '    Course_ID VARCHAR(15),'
                '    Class_ID CHAR(5),'
                '    Room_Number INTEGER,'
                '    Address VARCHAR(100),'
-               '    PRIMARY KEY(Course_ID, Class_ID, Room_Number, Address)'
-               ')'
-               'CREATE TABLE TaughtBy('
+               '    PRIMARY KEY(Course_ID, Class_ID, Room_Number, Address),'
+               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID),'
+               '    FOREIGN KEY(Class_ID) REFRENCES Class(Class_ID),'
+               '    FOREIGN KEY(Room_Number) REFRENCES Room(Room_Number),'
+               '    FOREIGN KEY(Address) REFRENCES Building(address)'
+               ')')
+connection.commit()               
+cursor.execute('CREATE TABLE TaughtBy('
                '    ID INTEGER,'
                '    Class_ID CHAR(5),'
                '    Course_ID VARCHAR(15),'
-               '    PRIMARY KEY(ID, Class_ID, Course_ID)'
-               ')'
-               'CREATE TABLE Requires('
+               '    PRIMARY KEY(ID, Class_ID, Course_ID),'
+               '    FOREIGN KEY(ID) REFRENCES Instructor(ID),'
+               '    FOREIGN KEY(Class_ID) REFRENCES Class(Class_ID),'
+               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               ')')
+connection.commit()               
+cursor.execute('CREATE TABLE Requires('
                '    ID INTEGER,'
                '    Course_ID VARCHAR(15),'
-               '    PRIMARY KEY(ID, Course_ID)'
-               ')'
-               'CREATE TABLE EmergencyContact('
+               '    PRIMARY KEY(ID, Course_ID),'
+               '    FOREIGN KEY(ID) REFRENCES Student(ID),'
+               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               ')')
+connection.commit()               
+cursor.execute('CREATE TABLE EmergencyContact('
                '    Phone_Number CHAR(10),'
                '    Address VARCHAR(100),'
                '    Name VARCHAR(30),'
                '    Relation VARCHAR(30),'
                '    ID INTEGER,'
                '    PRIMARY KEY(ID, Name)'
-               ')'
-               'CREATE TABLE Department('
+               ')')
+connection.commit()               
+cursor.execute('CREATE TABLE Department('
                '    Program VARCHAR(30) PRIMARY KEY'
-               ')'
-                )
+               ')')
 connection.commit()
 cursor.close()
