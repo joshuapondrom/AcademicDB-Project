901fd8d3627db43badec6b4d06f12e236d7639c6
joshuapondrom <japrkf@mst.edu>
2017-12-03 16:31:55 -0600

    fixed sql init issues

--- initialize_uads.py
+++ initialize_uads.py
@@ -11,22 +11,22 @@ connection.commit()
 cursor.execute('CREATE TABLE Building('
                '    address VARCHAR(30) PRIMARY KEY,'
                '    Name VARCHAR(30),'
-               '    Abbr VARCHAR(5)
-               ')'
+               '    Abbr VARCHAR(5)'
+               ')')
 cursor.execute('CREATE TABLE Instructor('
                '    ID INTEGER PRIMARY KEY,'
                '    address VARCHAR(100),'
                '    Name VARCHAR(30),'
                '    Dept VARCHAR(30),'
-               '    FOREIGN KEY(Dept) REFRENCES Department(Program)'
+               '    FOREIGN KEY(Dept) REFERENCES Department(Program)'
                ')')
 connection.commit()
 cursor.execute('CREATE TABLE Located('
                '    Program VARCHAR(30),'
                '    Address VARCHAR(100),'
                '    PRIMARY KEY(Program,Address),'
-               '    FOREIGN KEY(Program) REFRENCES Department(Program),'
-               '    FOREIGN KEY(Address) REFRENCES Building(address)'
+               '    FOREIGN KEY(Program) REFERENCES Department(Program),'
+               '    FOREIGN KEY(Address) REFERENCES Building(address)'
                ')')
 connection.commit()
 cursor.execute('CREATE TABLE Room('
@@ -34,7 +34,7 @@ cursor.execute('CREATE TABLE Room('
                '    Room_Number INTEGER,'
                '    Address VARCHAR(100),'
                '    PRIMARY KEY(Room_Number, Address),'
-               '    FOREIGN KEY(Address) REFRENCES Building(address)'
+               '    FOREIGN KEY(Address) REFERENCES Building(address)'
                ')')
 connection.commit()
 cursor.execute('CREATE TABLE Class('
@@ -43,7 +43,7 @@ cursor.execute('CREATE TABLE Class('
                '    Time INTEGER,'
                '    Course_ID VARCHAR(15),'
                '    PRIMARY KEY(Course_ID, Class_ID),'
-               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               '    FOREIGN KEY(Course_ID) REFERENCES Course(Course_ID)'
                ')')
 connection.commit()
 cursor.execute('CREATE TABLE Course('
@@ -58,21 +58,21 @@ cursor.execute('CREATE TABLE Takes('
                '    Course_ID VARCHAR(15),'
                '    Grade FLOAT,'
                '    PRIMARY KEY(ID, Class_ID, Course_ID),'
-               '    FOREIGN KEY(ID) REFRENCES Student(ID),'
-               '    FOREIGN KEY(Class_ID) REFRENCES Class(Class_ID),'
-               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               '    FOREIGN KEY(ID) REFERENCES Student(ID),'
+               '    FOREIGN KEY(Class_ID) REFERENCES Class(Class_ID),'
+               '    FOREIGN KEY(Course_ID) REFERENCES Course(Course_ID)'
                ')')
 connection.commit()               
-cursor.execute('CREATE TABLE In('
+cursor.execute('CREATE TABLE ClassInRoom('
                '    Course_ID VARCHAR(15),'
                '    Class_ID CHAR(5),'
                '    Room_Number INTEGER,'
                '    Address VARCHAR(100),'
                '    PRIMARY KEY(Course_ID, Class_ID, Room_Number, Address),'
-               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID),'
-               '    FOREIGN KEY(Class_ID) REFRENCES Class(Class_ID),'
-               '    FOREIGN KEY(Room_Number) REFRENCES Room(Room_Number),'
-               '    FOREIGN KEY(Address) REFRENCES Building(address)'
+               '    FOREIGN KEY(Course_ID) REFERENCES Course(Course_ID),'
+               '    FOREIGN KEY(Class_ID) REFERENCES Class(Class_ID),'
+               '    FOREIGN KEY(Room_Number) REFERENCES Room(Room_Number),'
+               '    FOREIGN KEY(Address) REFERENCES Building(address)'
                ')')
 connection.commit()               
 cursor.execute('CREATE TABLE TaughtBy('
@@ -80,17 +80,17 @@ cursor.execute('CREATE TABLE TaughtBy('
                '    Class_ID CHAR(5),'
                '    Course_ID VARCHAR(15),'
                '    PRIMARY KEY(ID, Class_ID, Course_ID),'
-               '    FOREIGN KEY(ID) REFRENCES Instructor(ID),'
-               '    FOREIGN KEY(Class_ID) REFRENCES Class(Class_ID),'
-               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               '    FOREIGN KEY(ID) REFERENCES Instructor(ID),'
+               '    FOREIGN KEY(Class_ID) REFERENCES Class(Class_ID),'
+               '    FOREIGN KEY(Course_ID) REFERENCES Course(Course_ID)'
                ')')
 connection.commit()               
 cursor.execute('CREATE TABLE Requires('
                '    ID INTEGER,'
                '    Course_ID VARCHAR(15),'
                '    PRIMARY KEY(ID, Course_ID),'
-               '    FOREIGN KEY(ID) REFRENCES Student(ID),'
-               '    FOREIGN KEY(Course_ID) REFRENCES Course(Course_ID)'
+               '    FOREIGN KEY(ID) REFERENCES Student(ID),'
+               '    FOREIGN KEY(Course_ID) REFERENCES Course(Course_ID)'
                ')')
 connection.commit()               
 cursor.execute('CREATE TABLE EmergencyContact('
