11869c0e4afaf073ca54f047c1b04ac5520bcfeb
joshuapondrom <japrkf@mst.edu>
2017-12-03 20:24:40 -0600

    Added form for submitting classes

--- webpage.py
+++ webpage.py
@@ -62,3 +62,32 @@ def classes():
 
     rows = curr.fetchall();
     return render_template("classes.html", rows = rows)
+
+@app.route('/enterclass')
+def enterclass():
+    return render_template('addclass.html')
+
+@app.route('/addclass',methods = ['POST', 'GET'])
+def addclass():
+   if request.method == 'POST':
+      try:
+         Class_ID = request.form['Class_ID']
+         DayCode = request.form['DayCode']
+         Time = request.form['Time']
+         Course_ID = request.form['Course_ID']
+         
+         with sql.connect("data.db") as db:
+            curr = db.cursor()
+            
+            curr.execute("""INSERT INTO Class(Class_ID,DayCode,Time,Course_ID) 
+               VALUES (?,?,?,?)""",(Class_ID,DayCode,Time,Course_ID) )
+            
+            db.commit()
+            msg = "Record successfully added"
+      except:
+         db.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         db.close()
